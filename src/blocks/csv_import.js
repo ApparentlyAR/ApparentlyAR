fc31e66c152ca5aa034a3d1e4bb078f696ac7472
BenGhahramani <benjamin.ghahramani@gmail.com>
2025-08-07 19:25:59 +1000

    foundation for import csv block, does not handle parsing.

---
+++ src/blocks/csv_import.js
@@ -0,0 +1,17 @@
+Blockly.defineBlocksWithJsonArray([
+  {
+    "type": "csv_import",
+    "message0": "import CSV %1",
+    "args0": [
+      {
+        "type": "field_button",
+        "name": "CSV_UPLOAD",
+        "text": "Choose File"
+      }
+    ],
+    "output": "Dataset",
+    "colour": 230,
+    "tooltip": "Import a CSV file as a dataset.",
+    "helpUrl": ""
+  }
+]); 
